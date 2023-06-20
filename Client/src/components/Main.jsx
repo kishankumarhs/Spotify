506952936cca163c337d3ab06263735404bbfa39
Kishan Kumar <hskishanraj@gmail.com>
2023-06-20 19:18:14 +0530

    Added Navbar & Top Nav

--- Client/src/components/Main.jsx
+++ Client/src/components/Main.jsx
@@ -1,9 +1,24 @@
 import { Outlet } from "react-router-dom"
+import { RightOutlined, LeftOutlined } from '@ant-design/icons'
+import './main.css'
 
 const Main = () => {
     return (
-        <div>
-            Main
+        <div className="main">
+            <div className="main-top">
+                <div className="navigation-arrows">
+                    <div className="forward-arrow">
+                        <LeftOutlined />
+                    </div>
+                    <div className="back-arrow">
+                        <RightOutlined />
+                    </div>
+                </div>
+                <div className="buttons-container">
+                    <button className="sign-up">sign up</button>
+                    <button className="login">login</button>
+                </div>
+            </div>
             <Outlet />
         </div>
 
--- Client/src/components/global/Navbar.jsx
+++ Client/src/components/global/Navbar.jsx
@@ -1,5 +1,6 @@
 import './navbar.css'
-import { HomeFilled, SearchOutlined } from '@ant-design/icons'
+import { HomeFilled, SearchOutlined, AppstoreAddOutlined, GlobalOutlined, PlusOutlined } from '@ant-design/icons'
+import { IconButton } from '@mui/material'
 
 
 const Navbar = () => {
@@ -8,17 +9,72 @@ const Navbar = () => {
             <div className="navigation">
                 <ul className="nav-list">
                     <li className="nav-items">
-                        <HomeFilled size={30} />
+                        <HomeFilled style={{
+                            fontSize: 20
+                        }} />
                         <span>Home</span>
                     </li>
                     <li className="nav-items">
-                        <SearchOutlined />
+                        <SearchOutlined style={{
+                            fontSize: 20
+                        }} />
                         <span>Search</span>
                     </li>
                 </ul>
             </div>
             <div className="your-library">
-
+                <div className="library-top">
+                    <AppstoreAddOutlined style={{
+                        fontSize: 20
+                    }} />
+                    <span>
+                        Your Library
+                    </span>
+                    <IconButton
+                        style={{
+                            marginLeft: "auto"
+                        }}
+                    >
+                        <PlusOutlined style={{ color: "#fff" }} />
+                    </IconButton>
+                </div>
+                <div className="create-playlist-container">
+                    <div className="create-playlist">
+                        <p className="playlist-head">
+                            Create your fist playlist
+                        </p>
+                        <p className="playlist-desc">
+                            Lorem ipsum dolor sit amet consectetur.
+                        </p>
+                        <button>
+                            create playlist
+                        </button>
+                    </div>
+                    <div className="find-podcast">
+                        <p className="podcast-head">
+                            Find some podcast
+                        </p>
+                        <p className="podcast-desc">
+                            Lorem ipsum dolor sit amet consectetur.
+                        </p>
+                        <button>
+                            Browse podcast
+                        </button>
+                    </div>
+                </div>
+                <div className="navbar-footer">
+                    <div className="nav-footer-links">
+                        <p className="nav-footer-link">Lorem   </p>
+                        <p className="nav-footer-link">ipsum</p>
+                        <p className="nav-footer-link">dolor</p>
+                        <p className="nav-footer-link">consectetur</p>
+                        <p className="nav-footer-link">voluptas</p>
+                    </div>
+                    <button className="nav-footer-button">
+                        <GlobalOutlined />
+                        English
+                    </button>
+                </div>
             </div>
 
         </div>
