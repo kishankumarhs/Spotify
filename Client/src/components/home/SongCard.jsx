8f12f2e7aa841483d0c2c6c8df972ab41ddd5606
Soumya <soumya.bsvr@gmail.com>
2023-06-22 15:26:52 +0530

    home page

--- Client/src/components/home/SongCard.jsx
+++ Client/src/components/home/SongCard.jsx
@@ -1,8 +1,30 @@
+import { PlayCircleFilled } from "@ant-design/icons"
 import "./SongCard.css"
 
 const SongCard = () => {
     return(
-        <div className="song-card">hai</div>
+        <div className="song-card">
+            <div className="play-button">
+                <PlayCircleFilled style={{
+                    fontSize: 40,
+                    color: "#089e1e"
+
+                }}/>
+            </div>
+            <div className="song-card-image-holder">
+                <img src="https://i.scdn.co/image/ab67706f00000002ca5a7517156021292e5663a6" alt="" />
+
+            </div>
+            <div className="song-card-info">
+                <p className="song-card-title">
+                    peaceful piano
+                </p>
+                <p className="song-card-description">
+                    Peaceful piano to help you slow down breath,stress buster and clam you brain 
+                </p>
+            </div>
+        </div>
+
     )
 }
 
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,10 +1,43 @@
+import SongCard from "../components/home/SongCard"
 import "./home.css"
 
 const Home = ()=> {
     return(
-        <div>
-            hai
-        </div>
+        <main className="home">
+            <section className="first-section">
+                <div className="section-top">
+                    <p className="section-head">Focus</p>
+                    <a href="" className="show-all">show all</a>
+                </div>
+                <div className="song-card-container">
+                    {
+                        [1, 23, 3, 4, 5].map(ele => <SongCard key={ele} />)
+                    }
+                </div>
+            </section>
+            <section className="first-section">
+                <div className="section-top">
+                    <p className="section-head">Spotify playlist</p>
+                    <a href="" className="show-all">show all</a>
+                </div>
+                <div className="song-card-container">
+                    {
+                        [1, 23, 3, 4, 5].map(ele => <SongCard key={ele} />)
+                    }
+                </div>
+            </section>
+            <section className="first-section">
+                <div className="section-top">
+                    <p className="section-head">Sound of India</p>
+                    <a href="" className="show-all">show all</a>
+                </div>
+                <div className="song-card-container">
+                    {
+                        [1, 23, 3, 4, 5].map(ele => <SongCard key={ele} />)
+                    }
+                </div>
+            </section>
+        </main>
     )
 }
 
